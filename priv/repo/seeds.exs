23924471da6b3354733c675acc820ec1e1c0987c
Jack Middlebrook <jack.middlebrook@gmail.com>
2024-03-31 10:48:28 +0800

    Add seed products

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,26 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Pento.Repo.insert!(%Pento.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "A game of strategy",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Tic-Tac-Toe",
+    description: "The game of Xs and Os",
+    sku: 1_234_567,
+    unit_price: 5.00
+  },
+  %{
+    name: "Table Tennis",
+    description: "A game of reflexes",
+    sku: 9_876_543,
+    unit_price: 15.00
+  }
+]
+
+Enum.each(products, fn product ->
+  Catalog.create_product(product)
+end)
